901c173f18c50e8664e4e6f77cc6cd407f6e62e7
Felipe <antoniofelipesilva@live.com>
2023-03-20 15:49:52 -0300

    version.1.0.5

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,16 +1,29 @@
 import React from "react";
-import { ScrollView } from 'react-native';
 import * as C from './style';
 
 
 export const Login = () => {
   return (
-    <ScrollView>
       <C.Container>
+        <C.ContainerLogo>
+          <C.TextLogo>
+            Seja Bem Vindo a Night Patrol!
+          </C.TextLogo>
+        </C.ContainerLogo>
         <C.ContainerForm>
-          <C.Input>Teste</C.Input>
+          <C.InputArea>
+            <C.TextArea>Email:</C.TextArea>
+            <C.Input placeholder='Digite seu email' />
+          </C.InputArea>
+          <C.InputArea>
+            <C.TextArea>Senha:</C.TextArea>
+            <C.Input placeholder='Digite sua senha' />
+          </C.InputArea>
+          <C.Button title="Login"/>
         </C.ContainerForm>
+        <C.ToRegister>
+          Faca seu cadastro!
+        </C.ToRegister>
       </C.Container>
-    </ScrollView>
   )
 }
--- src/screens/Login/style.ts
+++ src/screens/Login/style.ts
@@ -2,16 +2,60 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
+  background: ${({theme}) => theme.COLORS.BACKGROUND};
   justify-content: center;
   align-items: center;
 `
+export const ContainerLogo = styled.View`
+  display: flex;
+  width: 100%;
+  height: 50px;
+  justify-content: center;
+  align-items: center;
+`
+
+export const TextLogo = styled.Text`
+  font-size: 20px;
+  color: ${({theme}) => theme.COLORS.TEXT_PRIMARY};
+`
+
 export const ContainerForm = styled.View`
   width: 100%;
-  height: 100%;
+  height: 400px;
   justify-content: center;
   align-items: center;
 `
+export const InputArea = styled.View`
+  display: flex;
+  width: 100%;
+  height: 60px;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 10px;
+  margin-bottom: 20px;
+`
+
+export const TextArea = styled.Text`
+  font-size: 20px;
+  color: ${({ theme }) => theme.COLORS.TEXT_PRIMARY};
+`
+
 export const Input = styled.TextInput`
-  width: 200px;
+  color: ${({ theme }) => theme.COLORS.TEXT_PRIMARY};
+  text-align: center;
+  width: 300px;
   height: 30px;
+  border: 1px solid ${({ theme }) => theme.COLORS.PRIMARY_900};
+  border-radius: 20px;
+  margin-top: 5px;
+`
+export const Button = styled.Button`
+  width: 50px;
+  height: 30px;
+  border-radius: 10px;
+`
+export const ToRegister = styled.Text`
+  font-size: 12px;
+  color: ${({ theme }) => theme.COLORS.TEXT_PRIMARY};
 `
