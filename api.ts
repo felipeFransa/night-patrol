ebcf8742ea7b392d628c65165101d544d42a2afd
Felipe <antoniofelipesilva@live.com>
2023-03-23 17:05:24 -0300

    commit add api req

--- api.ts
+++ api.ts
@@ -1,11 +1,17 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: 'https://jsonplaceholder.typicode.com/',
+  baseURL: 'https://jsonplaceholder.typicode.com',
   timeout: 1000,
   headers: {'X-Custom-Header': 'foobar'}
 });
 
 export const api = {
-  
+  getOneUser: async () => {
+    let response = await http.get('/users');
+    return response.data;
+  },
+  addNewUsers: async (name: string, email: string, password: string) => {
+    let response = await http.post('/users')
+  }
 }
