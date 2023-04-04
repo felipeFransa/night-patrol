6a34d4701c5dd5adc3740ba9a4a0116c686f2a76
Felipe <antoniofelipesilva@live.com>
2023-04-04 17:59:06 -0300

    version.1.3.5

--- src/routes/tab.routes.tsx
+++ src/routes/tab.routes.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Home } from '../screens/Home';
 import { Toppings } from '../screens/Toppings';
+import { Activities } from  '../screens/Activities';
 
 const Tab = createBottomTabNavigator();
 
 export default () => {
  return (
   <Tab.Navigator>
-  <Tab.Screen name="Home" component={Home}/>
-  <Tab.Screen name="Toppings" component={Toppings}/>
+    <Tab.Screen name="Home" component={Home}/>
+    <Tab.Screen name="Toppings" component={Toppings}/>
+    <Tab.Screen name="Atividades" component={Activities}/>
   </Tab.Navigator>
  )
 }
