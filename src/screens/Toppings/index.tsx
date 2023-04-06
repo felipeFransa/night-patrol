2978814c591c5e65c8f849eb233877c6ab198842
Felipe <antoniofelipesilva@live.com>
2023-04-06 18:03:47 -0300

    version.1.4.2

--- src/screens/Toppings/index.tsx
+++ src/screens/Toppings/index.tsx
@@ -1,25 +1,54 @@
-import { useNavigation } from '@react-navigation/native'
+import React, { useState } from 'react';
 import * as C from './style';
 
+const imgStart = require('../../assets/ciclismo.png');
+const imgStop = require('../../assets/patrolStop.png');
+
+const Users = {
+  firstName: 'Felipe',
+  address: 'Caicara',
+  phoneNumber: '31-9171228890',
+}
+const dateAtual = '06/04/2023'
+
+const patrolNTF = [
+  {local: Users.address, hora: '21:00', IMG: imgStart, text: 'Ronda Inicia!', date: dateAtual},
+  {local: Users.address, hora: '21:40', IMG: imgStop, text: 'Ronda Fechada!',  date: dateAtual},
+  {local: Users.address, hora: '21:45', IMG: imgStart,  text: 'Ronda Inicia!', date: dateAtual},
+  {local: Users.address, hora: '22:25', IMG: imgStop, text: 'Ronda Fechada!', date: dateAtual},
+  {local: Users.address, hora: '23:30', IMG: imgStart,  text: 'Ronda Inicia!', date: dateAtual},
+  {local: Users.address, hora: '00:00', IMG: imgStop, text: 'Ronda Fechada!', date: dateAtual},
+]
+ 
 export const Toppings = () => {
 
-  const navigation = useNavigation();
-
-  const handleGoHome = () => {
-    navigation.navigate('Home')
-  }
-  const handleGoTopping = () => {
-    navigation.navigate("Toppings")
-  }
-  const handleGoActivities = () => {
-    navigation.navigate("Activities")
-  }
   return (
     <C.Container>
 
-      <C.ToppingsBox>
-        <C.Toppings></C.Toppings>
-      </C.ToppingsBox>
+      <C.BoxNotification>
+        <C.NotificationTitle>
+          <C.TitleNotification>Ultimas chamadas</C.TitleNotification>
+          <C.TextSecudary>Ver tudo!</C.TextSecudary>
+        </C.NotificationTitle>
+
+        <C.BoxNotificationArea>
+            {patrolNTF.map((x, index) =>(
+              <C.NotificationBody key={index}>
+                <C.LogoBodyNTF>
+                  <C.PatrolIMG source={x.IMG}/>
+                </C.LogoBodyNTF>
+                <C.BodyNFT>
+                  <C.ContentBodyNFT>{x.text}</C.ContentBodyNFT>
+                  <C.ContentBodyNFTDate>{x.date}</C.ContentBodyNFTDate>
+                </C.BodyNFT>
+                <C.HoursBody>
+                  <C.HoursTxt>{x.hora}</C.HoursTxt>
+                </C.HoursBody>
+              </C.NotificationBody>
+            ))}
+        </C.BoxNotificationArea>
+        
+      </C.BoxNotification>
 
     </C.Container>
   )
--- src/screens/Toppings/style.ts
+++ src/screens/Toppings/style.ts
@@ -7,18 +7,74 @@ export const Container = styled.SafeAreaView`
   justify-content: space-around;
   flex-direction: column;
 `
-export const ToppingsBox = styled.View`
-  width: 320px;
-  height: 500px;
+export const BoxNotification = styled.View`
   display: flex;
-  justify-content: space-around;
+  width: 100%;
+  height: auto;
+  justify-content: center;
   align-items: center;
   flex-direction: column;
 `
+export const BoxNotificationArea = styled.ScrollView`
+`
 
-export const Toppings = styled.View`
-  width: 320px;
+export const NotificationTitle = styled.View`
+  width: 100%;
+  height: 30px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`
+
+export const TitleNotification = styled.Text`
+  font-size: 16px;
+  color: ${({theme})=> theme.COLORS.TEXT_PRIMARY};
+`
+export const TextSecudary = styled.Text`
+  font-size: 12px;
+  color: ${({ theme }) => theme.COLORS.TEXT_SECONDARY};
+`
+
+export const NotificationBody = styled.View`
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`
+
+export const LogoBodyNTF = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50px;
   height: 50px;
-  background: ${({theme})=>theme.COLORS.TEXT_SECONDARY};
-  border-radius: 20px;
+`
+export const PatrolIMG = styled.Image`
+  width: 32px;
+  height: 32px;
+`
+export const BodyNFT = styled.View`
+  display: flex;
+  flex-direction: column;
+`
+export const ContentBodyNFT = styled.Text`
+  font-size: 16px;
+  color: ${({ theme }) => theme.COLORS.TEXT_PRIMARY};
+`
+export const ContentBodyNFTDate = styled.Text`
+  font-size: 12px;
+  color: ${({ theme }) => theme.COLORS.TEXT_SECONDARY};
+`
+export const HoursBody = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 40px;
+  height: 30px;
+  background: ${({ theme }) => theme.COLORS.BACKGROUND};
+`
+export const HoursTxt = styled.Text`
+  font-size: 16px;
+  color: ${({ theme }) => theme.COLORS.TEXT_PRIMARY};
 `
