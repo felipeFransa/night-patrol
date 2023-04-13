9db610bebd69750982dcad62ad6515ebf8ddd02b
Felipe <antoniofelipesilva@live.com>
2023-04-13 18:31:42 -0300

    version.1.6.1

--- src/screens/StartPatrol/index.tsx
+++ src/screens/StartPatrol/index.tsx
@@ -1,24 +1,43 @@
 import React, { useState, useEffect} from 'react';
+import { Toast } from 'react-native-toast-message/lib/src/Toast';
+import uuid from 'react-native-uuid';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as c from './style';
 
 export const StartPatrol = () => {
-  const [localization, setLocation] = useState('Caicaira');
-  const [hours, setHours] = useState('');
-  const [date, setDate] = useState('');
+  const [userAdmin, setUserAdmin] = useState('Felipe');
+  const [userAddress, setUserAddress] = useState('Caiçaras');
+  const [nameBD, setNameBD] = useState(`${userAdmin}-${userAddress}`);
   const [start, setStart] = useState(false);
-  const [patrolStart, setPatrolStart] = useState('')
   const [called, setCalled] = useState(false);
 
-  const handleNewDate = () => {
-    const data = new Date(Date.now());
-    const NewDate = `${data.getUTCDate()}/${data.getUTCDay()}/${data.getUTCFullYear()}`
-    const NewHours = `${data.getHours()}:${data.getMinutes()}`;
-    setHours(NewHours)
-    setDate(NewDate);
-    setPatrolStart(`Ronda iniciada as ${hours} do dia ${date} em  ${localization}`);
+  const HandleNewPatrol = async () => {
+    const id = uuid.v4();
+    const Data = new Date();
+    const Day = `${Data.getDate()}/${Data.getDay()}/${Data.getFullYear()}`;
+    const Hours = `${Data.getHours()}:${Data.getMinutes()}`;
+
+
+    const newStartPatrol = {
+      _id: id,
+      title: 'Ronda Iniciada',
+      data: Day,
+      horas: Hours,
+      newDate: new Date()
+    }
+
+    await AsyncStorage.setItem(nameBD, JSON.stringify(newStartPatrol));
+
+    console.log(newStartPatrol);
+
+    Toast.show({
+      type: 'success',
+      text1: 'Ronda Iniciada'
+    });
   }
+
   useEffect(()=>{
-    handleNewDate();
+   
   },[])
 
   return (
@@ -26,32 +45,32 @@ export const StartPatrol = () => {
       {!start && 
         <c.ContainerSP>
           <c.BoxText>
-            <c.Text>{localization}</c.Text>
+            <c.Text>{userAddress}</c.Text>
           </c.BoxText>
           <c.BoxText>
-            <c.Text>{hours}</c.Text>
+            <c.Text>{}</c.Text>
           </c.BoxText>
           <c.BoxText>
-            <c.Text>{date}</c.Text>
+            <c.Text>{}</c.Text>
           </c.BoxText>
         </c.ContainerSP>
       }
 
       <c.BoxStartNotification>
         <c.MSGStartNotification>
-          <c.MSGStart>{patrolStart}</c.MSGStart>
+          <c.MSGStart></c.MSGStart>
         </c.MSGStartNotification>
         {called &&
         <c.MSGStartNotification>
-          <c.MSGStart>Cobertura as {hours}.</c.MSGStart>
+          <c.MSGStart></c.MSGStart>
         </c.MSGStartNotification>
          }
         <c.MSGStartNotification>
           <c.MSGStart></c.MSGStart>
         </c.MSGStartNotification>
       {!start && 
-        <c.BTNSend>
-          <c.BTNText onPress={()=>setStart(true)}>Iniciar</c.BTNText>
+        <c.BTNSend onPress={HandleNewPatrol}>
+          <c.BTNText>Iniciar</c.BTNText>
         </c.BTNSend>
       }
       </c.BoxStartNotification>
