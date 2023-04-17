b1d7fcce850f6089f2ba61bade83b8e63c4481ed
Felipe <antoniofelipesilva@live.com>
2023-04-17 18:03:31 -0300

    version.1.7.2

--- App.tsx
+++ App.tsx
@@ -2,7 +2,16 @@ import 'react-native-gesture-handler';
 import { useState } from 'react';
 import { ThemeProvider } from 'styled-components/native';
 import { AuthProvider } from './src/context/Auth';
+import * as Notifications from 'expo-notifications';
 import { ThemeBTN, TextBTN } from './styleGlobal'
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 import Router from './src/routes/Routes';
 
--- src/screens/Activities/index.tsx
+++ src/screens/Activities/index.tsx
@@ -1,13 +1,20 @@
 import * as C from './style';
+import MapView from 'react-native-maps';
 
 export const Activities = () => {
 
   
   return (
     <C.Container>
-      <C.NotificationBox>
-        <C.Notification></C.Notification>
-      </C.NotificationBox>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+    />
 
     </C.Container>
   )
