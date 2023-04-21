fa41b1563889a0d896c65f1fb2103a38ba14b6f5
Felipe <antoniofelipesilva@live.com>
2023-04-21 18:45:32 -0300

    version.1.7.6

--- src/screens/Activities/index.tsx
+++ src/screens/Activities/index.tsx
@@ -1,20 +1,41 @@
+import React, { useState, useEffect } from 'react';
 import * as C from './style';
-import MapView from 'react-native-maps';
+import { requestForegroundPermissionsAsync, getCurrentPositionAsync, LocationObject } from 'expo-location';
+import MapView, { Marker } from 'react-native-maps';
 
 export const Activities = () => {
+  const [location, setLocation] = useState<LocationObject | null>(null);
+
+  const requestLocationPermission = async () => {
+    const { granted } = await requestForegroundPermissionsAsync();
+    if( granted){
+      const currentPosition = await getCurrentPositionAsync();
+      setLocation(currentPosition)
+      console.log('Localizacao Atual:', currentPosition)
+    }
+  }
+  useEffect(()=>{
+    requestLocationPermission()
+  },[])
 
   
   return (
     <C.Container>
+      {location && 
       <MapView
-        style={{ flex: 1 }}
+        style={{ flex: 1, width: '100%', height: '100%' }}
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
+          latitude: location?.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
         }}
-    />
+      	>
+        <Marker coordinate={{
+          latitude: location?.coords.latitude,
+          longitude: location.coords.longitude,
+        }}/>
+      </MapView>}
 
     </C.Container>
   )
