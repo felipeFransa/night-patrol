bd163869530bd2170fe9c8f01fd9ed286f75493e
Felipe Fransa <antoniofelipesilva@live.com>
2024-06-27 18:48:18 -0300

    class client

---
+++ src/br/com/nightpatrol/model/Client.java
@@ -0,0 +1,31 @@
+package br.com.nightpatrol.model;
+
+public class Client {
+    private String firstName;
+    private Integer age;
+    private String City;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String city) {
+        City = city;
+    }
+}
