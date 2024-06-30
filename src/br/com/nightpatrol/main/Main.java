bdf5d18861cdafd14eeae0f56073e87b5f08f9aa
Felipe Fransa <antoniofelipesilva@live.com>
2024-06-30 20:48:05 -0300

    add main structure

--- src/br/com/nightpatrol/main/Main.java
+++ src/br/com/nightpatrol/main/Main.java
@@ -1,7 +1,14 @@
 package br.com.nightpatrol.main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner reading = new Scanner(System.in);
         System.out.println("Hello world!");
+        System.out.println("Enter your user");
+        String user = reading.nextLine();
+
+        System.out.println(user);
     }
 }
