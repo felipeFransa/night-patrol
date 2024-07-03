4fddc45c585487831c0e31fca21b6e3f83c64acd
Felipe Fransa <antoniofelipesilva@live.com>
2024-07-03 20:25:14 -0300

    add call text

--- src/br/com/nightpatrol/main/MainCall.java
+++ src/br/com/nightpatrol/main/MainCall.java
@@ -6,11 +6,13 @@ import java.util.Date;
 
 public class MainCall {
     public static void main(String[] args) {
+        System.out.println("olá seja bem vindo!");
         Call call = new Call();
 
         call.setDate(new Date());
         call.setHours(new Date());
 
+        System.out.println("chamada realizadar");
         System.out.println(call.getDate());
     }
 }
