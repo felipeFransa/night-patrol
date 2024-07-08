59849daffc4f5c065d069ff36141edcb34ec7065
Felipe Fransa <antoniofelipesilva@live.com>
2024-07-07 22:31:04 -0300

    scanner to main client

--- src/br/com/nightpatrol/main/MainClient.java
+++ src/br/com/nightpatrol/main/MainClient.java
@@ -2,11 +2,16 @@ package br.com.nightpatrol.main;
 
 import br.com.nightpatrol.model.Client;
 
+import java.util.Scanner;
+
 public class MainClient {
     public static void main(String[] args) {
+        Scanner reading = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = reading.nextLine();
         Client client = new Client();
 
-        client.setFirstName("felipe");
+        client.setFirstName(name);
         client.setAge(27);
         client.setCity("Belo horizonte");
 
