9790c80681728860e8defcde38d618d65d323fff
Felipe Fransa <antoniofelipesilva@live.com>
2024-07-10 17:18:25 -0300

    add scanner or main manager

--- src/br/com/nightpatrol/main/MainManager.java
+++ src/br/com/nightpatrol/main/MainManager.java
@@ -2,15 +2,31 @@ package br.com.nightpatrol.main;
 
 import br.com.nightpatrol.model.Manager;
 
+import java.util.Scanner;
+
 public class MainManager {
     public static void main(String[] args) {
+
+        Scanner readind = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String userManagerName = readind.nextLine();
+
+        System.out.println("enter your city");
+        String userManagerCity = readind.nextLine();
+
+        System.out.println("enter your region");
+        String userManagerRegion = readind.nextLine();
+
         Manager manager = new Manager();
 
-        manager.setFirstName("");
+        manager.setFirstName(userManagerName);
         manager.setAge(27);
-        manager.setCity("");
-        manager.setRegion("");
+        manager.setCity(userManagerCity);
+        manager.setRegion(userManagerRegion);
 
-        System.out.println(manager);
+        System.out.println(manager.getFirstName());
+        System.out.println(manager.getAge());
+        System.out.println(manager.getCity());
+        System.out.println(manager.getRegion());
     }
 }
