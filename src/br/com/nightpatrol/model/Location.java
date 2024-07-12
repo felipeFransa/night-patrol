9f7105b27ea75dd505ebf42cb0499fca06713a7f
Felipe Fransa <antoniofelipesilva@live.com>
2024-07-12 18:39:26 -0300

    add location

---
+++ src/br/com/nightpatrol/model/Location.java
@@ -0,0 +1,14 @@
+package br.com.nightpatrol.model;
+
+public class Location {
+    private String longitude;
+    private String latitude;
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+}
