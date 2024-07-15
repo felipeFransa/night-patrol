596d9dcab22179c5ee93c006f25ea0e8776a9b10
Felipe Fransa <antoniofelipesilva@live.com>
2024-07-15 18:06:59 -0300

    add location

--- src/br/com/nightpatrol/main/MainCall.java
+++ src/br/com/nightpatrol/main/MainCall.java
@@ -1,6 +1,7 @@
 package br.com.nightpatrol.main;
 
 import br.com.nightpatrol.model.Call;
+import br.com.nightpatrol.model.Location;
 
 import java.util.Date;
 
@@ -8,6 +9,10 @@ public class MainCall {
     public static void main(String[] args) {
         System.out.println("Hello welcome!");
         Call call = new Call();
+        Location location = new Location();
+
+        location.setLatitude("");
+        location.setLongitude("");
 
         call.setDate(new Date());
         call.setHours(new Date());
