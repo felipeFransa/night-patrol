158102dea70c6cbbf313e7e9de4bd47f9d79d70b
Felipe Fransa <antoniofelipesilva@live.com>
2024-07-16 19:19:02 -0300

    add main refactor

--- src/br/com/nightpatrol/main/Main.java
+++ src/br/com/nightpatrol/main/Main.java
@@ -1,7 +1,13 @@
 package br.com.nightpatrol.main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner reading = new Scanner(System.in);
         System.out.println("Hello welcome!");
+        System.out.println("choose an option");
+        String option = reading.nextLine();
+        System.out.println(option.getClass());
     }
 }
