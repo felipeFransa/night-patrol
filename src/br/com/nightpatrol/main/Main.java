711a2b3e9ecb854073de4a7480da2090824fb2d4
Felipe Fransa <antoniofelipesilva@live.com>
2024-07-23 16:12:42 -0300

    add conditionals

--- src/br/com/nightpatrol/main/Main.java
+++ src/br/com/nightpatrol/main/Main.java
@@ -1,17 +1,30 @@
 package br.com.nightpatrol.main;
 
+import br.com.nightpatrol.model.Manager;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner reading = new Scanner(System.in);
-        System.out.println("Hello welcome!");
+        Manager manager = new Manager();
+        manager.setFirstName("Felipe");
+        manager.setAge(27);
+
+        System.out.println("Hello welcome your bank");
         System.out.println("choose an option");
         Integer option = reading.nextInt();
 
 
         while (option != 0){
-            System.out.println("Access negado!");
+            if (option  == 1 ){
+                System.out.println("Seja bem vindo!");
+            } else if ( option == 2) {
+                System.out.println("Seja bem vindo " +manager.getFirstName());
+            } else {
+                System.out.println("acesso negado");
+            }
             break;
         }
     }
