6e715088ce636acd434cad8bbb609497557c5e40
Felipe Fransa <antoniofelipesilva@live.com>
2024-07-25 17:34:44 -0300

    add main call loop while

--- src/br/com/nightpatrol/main/MainCall.java
+++ src/br/com/nightpatrol/main/MainCall.java
@@ -1,6 +1,7 @@
 package br.com.nightpatrol.main;
 
 import br.com.nightpatrol.model.Call;
+import br.com.nightpatrol.model.Client;
 import br.com.nightpatrol.model.Location;
 
 import java.util.Date;
@@ -10,6 +11,12 @@ public class MainCall {
         System.out.println("Hello welcome!");
         Call call = new Call();
         Location location = new Location();
+        Client client = new Client();
+
+        client.setFirstName("Rodrigo");
+        client.setRua("F. Ovinio");
+        client.setNumberHouse(100);
+        client.setTypePlan(true);
 
         location.setLatitude("");
         location.setLongitude("");
@@ -17,8 +24,11 @@ public class MainCall {
         call.setDate(new Date());
         call.setHours(new Date());
 
-        System.out.println("Call success");
-        System.out.println(call.getDate());
-        System.out.println(call.getHours());
+
+        while (true){
+            System.out.println("Call success");
+            System.out.println(call.getDate());
+            break;
+        }
     }
 }
