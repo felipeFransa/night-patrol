5f3ffccc31d71d84e2639b040e124d3f97ea0003
Felipe Fransa <antoniofelipesilva@live.com>
2024-07-27 19:57:01 -0300

    add main call & client

--- src/br/com/nightpatrol/main/MainCall.java
+++ src/br/com/nightpatrol/main/MainCall.java
@@ -5,19 +5,33 @@ import br.com.nightpatrol.model.Client;
 import br.com.nightpatrol.model.Location;
 
 import java.util.Date;
+import java.util.Scanner;
 
 public class MainCall {
     public static void main(String[] args) {
         System.out.println("Hello welcome!");
+        Scanner reading = new Scanner(System.in);
         Call call = new Call();
         Location location = new Location();
         Client client = new Client();
 
-        client.setFirstName("Rodrigo");
-        client.setRua("F. Ovinio");
-        client.setNumberHouse(100);
+        System.out.println("enter your name:");
+        String firstName = reading.nextLine();
+
+        System.out.println("enter your rua:");
+        String rua = reading.nextLine();
+
+        System.out.println("enter your number house:");
+        Integer numberHouse  = reading.nextInt();
+
+        System.out.println("enter your cep:");
+        Integer cep = reading.nextInt();
+
+        client.setFirstName(firstName);
+        client.setRua(rua);
+        client.setNumberHouse(numberHouse);
         client.setTypePlan(true);
-        client.setCep(32113460);
+        client.setCep(cep);
 
         location.setLatitude("");
         location.setLongitude("");
