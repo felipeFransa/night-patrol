a1b53dd860fb1d8947ac783794371066912bf2cc
Felipe Fransa <antoniofelipesilva@live.com>
2024-08-23 18:59:55 -0300

    new class notice

---
+++ src/br/com/nightpatrol/model/Notice.java
@@ -0,0 +1,22 @@
+package br.com.nightpatrol.model;
+
+public class Notice {
+    private String noticeTitle;
+    private String noticeDescription;
+
+    public String getNoticeTitle() {
+        return noticeTitle;
+    }
+
+    public void setNoticeTitle(String noticeTitle) {
+        this.noticeTitle = noticeTitle;
+    }
+
+    public String getNoticeDescription() {
+        return noticeDescription;
+    }
+
+    public void setNoticeDescription(String noticeDescription) {
+        this.noticeDescription = noticeDescription;
+    }
+}
