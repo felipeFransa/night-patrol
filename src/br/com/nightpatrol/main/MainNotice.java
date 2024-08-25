931036beb91ac3dc6a1d1ce5062c47d78bd4edf7
Felipe Fransa <antoniofelipesilva@live.com>
2024-08-25 20:50:16 -0300

    add  new main notice

--- src/br/com/nightpatrol/main/MainNotice.java
+++ src/br/com/nightpatrol/main/MainNotice.java
@@ -1,7 +1,13 @@
 package br.com.nightpatrol.main;
 
+import br.com.nightpatrol.model.Notice;
+
 public class MainNotice {
     public static void main(String[] args) {
         System.out.println("main notice");
+        Notice notice = new Notice();
+
+        notice.setNoticeTitle("");
+        notice.setNoticeDescription("");
     }
 }
