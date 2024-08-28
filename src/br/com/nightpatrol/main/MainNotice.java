f07b1a73f3a3f58a198eac5def071f5385609ff5
Felipe Fransa <antoniofelipesilva@live.com>
2024-08-28 18:44:38 -0300

    main

--- src/br/com/nightpatrol/main/MainNotice.java
+++ src/br/com/nightpatrol/main/MainNotice.java
@@ -3,12 +3,14 @@ package br.com.nightpatrol.main;
 import br.com.nightpatrol.model.Notice;
 
 import java.time.LocalDate;
+import java.util.Date;
 
 public class MainNotice {
     public static void main(String[] args) {
         System.out.println("main notice");
         LocalDate myObj = LocalDate.now();
         Notice notice = new Notice();
+
 
         notice.setNoticeTitle("new notice");
         notice.setNoticeDescription("today's news");
@@ -18,5 +20,6 @@ public class MainNotice {
 
         System.out.println(title+ " or " +description);
         System.out.println(myObj);
+        System.out.println(new Date());
     }
 }
