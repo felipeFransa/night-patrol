8d73a60ee4edcbb2320803dc9a73780b2c17cadd
Felipe Fransa <antoniofelipesilva@live.com>
2024-08-31 20:23:57 -0300

    add new structure mainn notice

--- src/br/com/nightpatrol/main/MainNotice.java
+++ src/br/com/nightpatrol/main/MainNotice.java
@@ -4,15 +4,23 @@ import br.com.nightpatrol.model.Notice;
 
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.Scanner;
 
 public class MainNotice {
     public static void main(String[] args) {
         System.out.println("main notice");
+        Scanner reading = new Scanner(System.in);
+        System.out.println("enter new notice");
+        String titleNotice = reading.nextLine();
+
+        System.out.println("enter new description notice");
+        String descriptionNotice = reading.nextLine();
+
         LocalDate myObj = LocalDate.now();
         Notice notice = new Notice();
 
-        notice.setNoticeTitle("new notice");
-        notice.setNoticeDescription("today's news");
+        notice.setNoticeTitle(titleNotice);
+        notice.setNoticeDescription(descriptionNotice);
 
         String title = notice.getNoticeTitle();
         String description  = notice.getNoticeDescription();
